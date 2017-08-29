493f582a1859af10f3734fac9d7585225ae31a4a
Leon van der Velde <leon@paypro.nl>
2017-08-29 11:24:28 +0200

    Add comments

--- lib/paypro/client.rb
+++ lib/paypro/client.rb
@@ -1,5 +1,9 @@
 module PayPro
-  # Client class
+  # Client class to connect to the PayPro V1 API.
+  # Requires an API key to authenticate API calls, you
+  # can also supply your own Faraday connection instead of the default one.
+  # This can be useful if you want to add more middleware or want finer
+  # control of the connection being used.
   class Client
     attr_accessor :command, :params
 
@@ -9,6 +13,8 @@ module PayPro
       @conn = conn
     end
 
+    # Executes the API call and handles the response. Will raise errors if
+    # there were problems while connecting or handeling the response.
     def execute
       response = @conn.post do |req|
         req.body = body
@@ -18,6 +24,7 @@ module PayPro
       raise ConnectionError, "Could not connect to the PayPro API: #{e.inspect}"
     end
 
+    # Returns the body that is used in the POST request.
     def body
       {
         apikey: @api_key,
